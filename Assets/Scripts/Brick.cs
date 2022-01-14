8478c1b9d4f92d46a6f6488b5fb94e44e950e74c
EGCM <euclid0888@gmail.com>
2022-01-14 22:39:04 +0800

    Code Along Progress 2

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    public int hits = 1;
+    public int hits = 2;
     public int points = 10;
+    public Vector3 rotator;
+    public Material hitMaterial;
 
+    Material _origMaterial;
+    Renderer _renderer;
     void Start()
     {
-        
+        // transform.Rotate(rotator * (transform.position.x + transform.position.y) * 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _origMaterial = _renderer.sharedMaterial;
     }
 
     void Update()
     {
-        
+        // transform.Rotate(rotator * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter(Collision collision) {
         hits--;
+
         if (hits <= 0)
         {
             Destroy(gameObject);
         }
+
+        _renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterial", 0.05f);
+    }
+
+    private void RestoreMaterial()
+    {
+        _renderer.sharedMaterial = _origMaterial;
     }
 }
